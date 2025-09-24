83f5280f3dcbb69bb023f7e1b8962e49dd1678df
OverCaivano <overcaivano26@gmail.com>
2025-09-24 01:23:58 -0300

    Reubicacion de las estrategias(clases) en archivos distintos

--- strategies/auth_strategy.py
+++ strategies/auth_strategy.py
@@ -4,13 +4,3 @@ class AuthStrategy(ABC):
     @abstractmethod
     def authenticate(self, user, password) -> bool:
         pass
-
-
-class PasswordAuth(AuthStrategy):
-    def authenticate(self, user, password) -> bool:
-        return user.password == password
-
-
-class TokenAuth(AuthStrategy):
-    def authenticate(self, user, token) -> bool:
-        return user.token == token
